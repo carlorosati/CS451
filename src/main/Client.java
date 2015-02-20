4bddca14f97a906bc63a06963a60016040fb0cac
mirzael <michael.y.irizarry@gmail.com>
2015-02-19 19:47:09 -0500

    Updated Client so it's not using hardcoded localhost. Instead uses InetAddress

--- src/main/Client.java
+++ src/main/Client.java
@@ -1,12 +1,13 @@
 package main;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.Socket;
 
 public class Client {
 	
 	public static void main(String[] args) throws IOException{
 		System.out.println("Attempting to connect");
-		Socket sock = new Socket("localhost",4401);
+		Socket sock = new Socket(InetAddress.getByName("localhost"),4401);
 
 		System.out.println("Connected");
 		
