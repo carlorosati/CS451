90e0780dca4ab6d06f1c42bf12f39abec990ab46
Vincent Engard <vince.engard@gmail.com>
2015-03-05 23:51:44 -0500

    Updating files to incorporate imageicons

--- src/main/GUIChessBoard.java
+++ src/main/GUIChessBoard.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -20,12 +21,54 @@ public class GUIChessBoard extends JFrame implements MouseListener
 		this.board = board;
 		this.setLayout(new GridLayout(8,8));
 		
+		//Prepare guibuttons on board
 		guiBoardButtons = new JButton[8][8];
 		for(int x = 0; x < 8; x++){
 			for(int y = 0; y < 8; y++){
-				guiBoardButtons[x][y] = new JButton("Insert Chess Piece Here");
+				guiBoardButtons[x][y] = new JButton();
 				guiBoardButtons[x][y].setBackground(board.getChessSquare(x, y).getColor());
 				guiBoardButtons[x][y].addMouseListener(this);
+				
+				//Conditional for starting line up
+				//Setting the Pawn
+				if (y == 1){
+					this.board.getChessSquare(x,y).setChessPiece(new ChessPiecePawn(Color.BLACK, null, x, y));
+					//guiBoardButtons[x][y].setIcon(this.board.getChessSquare(new ChessPiecePawn(Color.BLACK, null, x, y))
+				}else if(y == 6){
+					this.board.getChessSquare(x,y).setChessPiece(new ChessPiecePawn(Color.WHITE, null, x, y));
+				//Every piece but the pawn
+				}else if(y == 0 || y == 7){
+					Color color; 
+					if(y == 0){
+						color = Color.BLACK;
+					}else{
+						color = Color.WHITE;
+					}
+					
+					//Setting the Rook
+					if (x == 0 || x == 7){
+						this.board.getChessSquare(x,y).setChessPiece(new ChessPieceRook(color, null, x, y));
+					
+					//Setting the Knight
+					}else if(x == 1 || x == 6){
+						this.board.getChessSquare(x,y).setChessPiece(new ChessPieceKnight(color, null, x, y));
+					
+					//Setting the Bishop
+					}else if(x == 2 || x == 5){
+						this.board.getChessSquare(x,y).setChessPiece(new ChessPieceBishop(color, null, x, y));
+					
+					//Setting the Queen
+					}else if(x == 3){
+						this.board.getChessSquare(x,y).setChessPiece(new ChessPieceKing(color, null, x, y));
+					
+					//Setting the King
+					}else if(x == 4){
+						this.board.getChessSquare(x,y).setChessPiece(new ChessPieceQueen(color, null, x, y));
+					}
+					
+					guiBoardButtons[x][y].setIcon(this.board.getChessSquare(x, y).getChessPiece().getRepresentation());
+				}
+				
 				if(this.board.getChessSquare(x, y).getChessPiece() != null && this.board.getChessSquare(x,y).getChessPiece().getRepresentation() != null){
 					guiBoardButtons[x][y].setIcon(this.board.getChessSquare(x, y).getChessPiece().getRepresentation());
 				}
